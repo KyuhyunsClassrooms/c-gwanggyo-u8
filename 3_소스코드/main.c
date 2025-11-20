a06ae856cb8d249a9f04f7ad211a0e036f459ddf
gwanggyo-u8 <gwanggyo.s240019@ggh.goe.go.kr>
2025-11-20 02:00:09 +0000

    소스 코드 완료 수정 필요

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -14,7 +14,7 @@ int main() {
 
     while (1)
     {
-        pintf("당신의 선택은?: ");
+        printf("당신의 선택은?: ");
         scanf("%d", &player);
 
         if(player==0)
@@ -25,10 +25,70 @@ int main() {
 
         if(player<1||player>3)
         {
-            pintf("잘못된 입력입니다. 1~3 중에서 선택해주세요.\n\n");
+            printf("잘못된 입력입니다. 1~3 중에서 선택해주세요.\n\n");
             continue;
         }
         
+        computer = rand() % 3 + 1;
+
+        printf("컴퓨터의 선택: ");
+        if(computer==1) printf("가위\n");
+        else if(computer==2) printf("바위\n");
+        else printf("보\n");
+
+        if(player==computer)
+        {
+            printf("비겼어요.\n");
+            printf("\n");
+            win=0;
+            lose=0;
+        }
+
+        else if((player==1&&computer==3)||(player==2&&computer==1)||(player==3&&computer==2))
+        {
+            printf("당신이 이겼어요!\n");
+            win++;
+            lose=0;
+
+            if(win==3)
+            {
+                printf("3연승 중! 정말 잘하는데요!\n");
+            }
+            else if(win==5)
+            {
+                printf("5연승 중! 가위바위보의 왕!\n");
+            }
+            else
+            {
+                printf("\n");
+            }
+
+        
+        }
+
+        else
+        {
+            printf("컴퓨터가 이겼어요.\n");
+            lose=++;
+            win=0;
+
+            if(lose==2)
+            {
+                printf("2 연패 중 운이 없으시네요\n");
+            }
+            else if(lose==3)
+            {
+                printf("3연패 힘내세요..\n");
+            }
+            else if(lose==5)
+            {
+                printf("정말 못 하시네요\n");
+            }
+            else
+            {
+                printf("\n");
+            }
+        }
     }
 
     
